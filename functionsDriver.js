b310762155278d673d3092abbb206de8ec96cc2f
Steve Lee <steve@opendirective.com>
2016-10-23 14:00:04 +0100

    Create functionsDriver.js

---
+++ functionsDriver.js
@@ -0,0 +1,29 @@
+const xs = require('xstream').default
+
+module.exports = function makeFunctionsDriver(context, dispwrap, inArgs) {
+
+    function driver(s$) {
+        s$.addListener({ next: (i) => { setTimeout(() => {  // next tick to allow other listenerns to be iterated
+                                            context.res = i 
+                                            if (dispwrap &&
+                                                typeof dispwrap.disposer === 'function') {
+                                                dispwrap.disposer()
+                                            }
+                                            context.done()
+                                            }, 1)
+                         },   
+                         error: () => {},
+                         complete: () => {}
+        })
+
+        return xs.createWithMemory({
+            start: listener => {
+                setTimeout(() => {listener.next({context, inArgs})}, 1)
+            },
+            stop: () => {},
+        })  
+    }
+
+    const logger = a => context.log(a)
+    return {driver, logger}
+}
