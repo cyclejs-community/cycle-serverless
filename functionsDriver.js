1626df556ed3e76deacee5e588a1fdf578fb1e96
Steve Lee <steve@opendirective.com>
2016-10-23 19:14:52 +0100

    create .editorconfig and reindent to 2 spaces

--- functionsDriver.js
+++ functionsDriver.js
@@ -2,28 +2,28 @@ const xs = require('xstream').default
 
 module.exports = function makeFunctionsDriver(context, inArgs, dispwrap) {
 
-    function driver(s$) {
-        s$.addListener({ next: (i) => { setTimeout(() => {  // next tick to allow other listenerns to be iterated
-                                            context.res = i 
-                                            if (dispwrap &&
-                                                typeof dispwrap.disposer === 'function') {
-                                                dispwrap.disposer()
-                                            }
-                                            context.done()
-                                            }, 1)
-                         },   
-                         error: () => {},
-                         complete: () => {}
-        })
+  function driver(s$) {
+    s$.addListener({ next: (i) => { setTimeout(() => {  // next tick to allow other listenerns to be iterated
+                                        context.res = i
+                                        if (dispwrap &&
+                                          typeof dispwrap.disposer === 'function') {
+                                          dispwrap.disposer()
+                                        }
+                                        context.done()
+                                      }, 1)
+                      },
+                      error: () => {},
+                      complete: () => {}
+    })
 
-        return xs.createWithMemory({
-            start: listener => {
-                setTimeout(() => {listener.next({context, inArgs})}, 1)
-            },
-            stop: () => {},
-        })  
-    }
+    return xs.createWithMemory({
+      start: listener => {
+          setTimeout(() => {listener.next({context, inArgs})}, 1)
+      },
+      stop: () => {},
+    })
+  }
 
-    const logger = a => context.log(a)
-    return {driver, logger}
+  const logger = a => context.log(a)
+  return {driver, logger}
 }
